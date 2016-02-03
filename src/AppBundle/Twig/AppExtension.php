fc7bd6cd8de783d87dd4f074cd6be4b9d6bbb543
busaev <busaev@gmail.com>
2016-02-03 22:48:29 +0300

    separated actions

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -6,13 +6,35 @@ class AppExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('actions', array($this, 'actions')),
+            new \Twig_SimpleFunction('actions', array($this, 'actions'), array(
+                'is_safe' => array('html'),
+                'needs_environment' => true
+            )),
         );
     }
 
-    public function actions($entityCode, $entity=false)
+    public function actions(\Twig_Environment $twig, $entityCode, $action='list', $entity=false)
     {
-        return $entityCode;
+        $globals = $twig->getGlobals();
+        
+        $tplPath = false;
+        
+        $defaultTemplatePath = $globals['kernel_root_dir'] . '/Resources/views/backend/entity/actions/default/'.$action.'.html.twig';
+        $entityTemplatePath  = $globals['kernel_root_dir'] . '/Resources/views/backend/entity/actions/'.$entityCode.'/'.$action.'.html.twig';
+        
+        if(file_exists($defaultTemplatePath))
+            $tplPath = $defaultTemplatePath;
+        
+        if(file_exists($entityTemplatePath))
+            $tplPath = $entityTemplatePath;
+        
+        if( ! $tplPath)
+            return '';
+        
+        return $twig->render($tplPath, [
+            'entityCode' => $entityCode,
+            'entity'     => $entity,
+        ]);
     }
 
     public function getName()
